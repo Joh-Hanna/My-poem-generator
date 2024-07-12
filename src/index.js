500c4a5529d728b83fd318731e7a0d3deda7f77e
Joh-Hanna <Johannaruhm@web.de>
2024-07-13 00:27:24 +0200

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ function generatePoem(event, result) {
     let userInput = document.querySelector("#user-input");
     let apiKey = "9240352440ao0101at80cbf93b35d00b";
     let context = "You are an AI poetrist who knows many poems about any topic and who excactly follows the users instructions.";
-    let prompt = `User instructions: Present me a short 5-6 lines poem about ${userInput.value}. Please use basic html format and make sure to seperate the lines with a <br>. Don't include a title or any words which are not belonging to the poem in your answer please.`;
+    let prompt = `User instructions: Present me a short 4-5 lines poem about ${userInput.value}. Please use basic html format and make sure to seperate the lines with a <br>. Don't include a title or any words which are not belonging to the poem in your answer please. Do NOT add an empty line below the poem.`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div id="generating">⏳Generating poem about ${userInput.value}, please wait...</div>`;
 
     axios.get(apiUrl).then(showNewPoem);
     return result 
 }
 
 function showNewPoem(result) {
+
+   
     new Typewriter("#poem", {
         strings: result.data.answer,
         autoStart: true,
